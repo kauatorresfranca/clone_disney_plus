0cb01ff36e4623674dea8d816347b67f0769211b
kaua torres <kauatorresfranca2@gmail.com>
2024-03-04 10:48:54 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-question]')
 
-    for (let i = 0; i< buttons.length; i++){
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY
+
+        if (posicaoAtual < alturaHero){
+            ocultaElementosDoHeader()
+        }else{
+            exibeElementosDoHeader()
+        }
+    })
+
+    // Sessão de atrações, prohramação das abas
+    for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
@@ -11,7 +26,31 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is--active')
         })
     }
+
+    //sessão faq, acordeon
+    for (let i =0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFrechaResposta)
+    }
 })
+
+function ocultaElementosDoHeader(){
+    const header = document.querySelector('.header')
+    header.classList.add('header--is--hidden')
+}
+
+function exibeElementosDoHeader(){
+    const header = document.querySelector('.header')
+    header.classList.remove('header--is--hidden')
+}
+
+
+function abreOuFrechaResposta(elemento){
+    const classe = 'faq__questions__item--is-open';
+    console.log(elemento)
+    const elementoPai = elemento.target.parentNode 
+
+    elementoPai.classList.toggle(classe)
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]')
